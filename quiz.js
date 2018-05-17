af82076b7b6c5eaa7f5235271506f6df05c5e55a
HARDIK CHUGH <31733278+hardy1334@users.noreply.github.com>
2018-05-17 19:20:57 +0530

    Changes made in this file

--- quiz.js
+++ quiz.js
@@ -8,15 +8,27 @@
 	*/
 
 	"use strict";
+    function displayDetails(){
+		var Student_ID = document.getElementById("Student_ID");
+		var studentId=document.getElementById("getid");
+		studentId.innerHTML= Student_ID.value;
+		var Student_Name=document.getElementById("firstname");
+		var studentName= document.getElementById("getname");
+		studentName.innerHTML=Student_Name.value;
+		var lastname = document.getElementById("lastname");
+		var lastName=document.getElementById("lastNameS");
+		lastName.innerHTML=lastname.value;
+	}
+
 	function validate(){
 		var errMsg = "";
 		var result = true;
 	var Student_ID = document.getElementById("Student_ID").value; 
 	var firstname = document.getElementById("firstname"). value;
 	var lastname = document.getElementById("lastname"). value;
-    var radio1 = document.getElementById("o1").checked;
-    var radio2 = document.getElementById("o2").checked;
-    var radio3 = document.getElementById("o3").checked;
+    var radio1 = document.getElementById("o1");
+    var radio2 = document.getElementById("o2");
+    var radio3 = document.getElementById("o3");
 	var isOption1 = document.getElementById("Option1").checked;
 	var isOption2 = document.getElementById("Option2").checked;
 	var isOption3 = document.getElementById("Option3").checked;
@@ -25,16 +37,16 @@
 	var Q5 = document.getElementById("Q5"). value;
 	var score = 0;
 	
+	//var getValueO3=document.getElementById("o3").value;
 	
-		
-	if (!firstname.match(/^[a-zA-Z]+$/)){
+		if (!firstname.match(/^[a-zA-Z]+$/)){
 		errMsg = errMsg + "your first name must only contain alpha characters\n"
 		result = false;
 		}
-	if(firstname == ("")){
+	    if(firstname == ("")){
 		errMsg = errMsg + "it cant be empty\n"
 		result = false;
-}
+        }
 		if (!lastname.match(/^[a-zA-Z]+$/)){
 		errMsg = errMsg + "Last name must contain alpha characters only\n"
 		}
@@ -42,7 +54,7 @@
 		if ((radio1=="")&&(radio2=="")&&(radio3=="")){
 				   errMsg = errMsg + "please select 1 option from q1\n"
 				   result = false;
-   }
+        }
 	/*atleast 1 option is selected */
 		if (!(isOption1 || isOption2 || isOption3 || isOption4)) {
 					errMsg = errMsg + "Please select one option from q3\n"
@@ -53,8 +65,8 @@
 		errMsg = errMsg + "error\	n"
 		result = false;	
 	
-	}	else if  (Q5 < 5) {
-			errMsg = errMsg + "noobs not allowed\n"
+	}	else if  (Q5 < 0) {
+		errMsg = errMsg + "Noobs not allowed \n"		
 			result = false;
 		}
 		else if (Q5 >= 10000) {
@@ -65,7 +77,7 @@
 	if (errMsg != ""){
 		alert(errMsg);
 	}
-	if (result) { storeBooking(Student_ID, firstname, lastname, radio1, radio2, radio3, isOption1, isOption2, isOption3, isOption4, Q4, Q5);
+	if (result) { storeBooking(Student_ID, firstname, lastname, radio1, radio2, radio3, isOption1, isOption2, isOption3, isOption4, Q4, Q5,score);
 	}
 	return result;
 	}
@@ -80,8 +92,36 @@
 	}
 	return getTime;
 	}	
-	
-	function storeBooking(Student_ID, firstname, lastname, radio1, radio2, radio3, isOption1, isOption2, isOption3, isOption4, Q4, Q5){ 	
+	function calcScore(){
+		var marks=0;
+		var radio3 = document.getElementById("o3");
+		var isOption3 = document.getElementById("Option3");
+		var Q5 = document.getElementById("Q5"). value;
+	var score=0;
+	if( radio3.checked ){
+	   
+		++score;
+		marks=score;
+	}
+
+		if(isOption3.checked){
+			++score;
+			marks=score;
+		}
+		if(Q5>70)
+		{
+			++score;
+			marks=score;
+		}
+
+		var getScore=document.getElementById("getscore");
+		getScore.innerHTML=marks;
+			
+	}
+
+
+
+	function storeBooking(Student_ID, firstname, lastname, radio1, radio2, radio3, isOption1, isOption2, isOption3, isOption4, Q4, Q5,score){ 	
 	var Q1 = "";
 	if(radio1){
 		Q1 = "radio1";
@@ -123,6 +163,7 @@
 	localStorage.Q3 = document.getElementById("Q3").value
 	localStorage.Q4 = document.getElementById("Q4").value;
 	localStorage.Q5 = document.getElementById("Q5").value;
+   	
 	}
 
 	function prefill_form(){
@@ -154,6 +195,7 @@
 	if(localStorage.Q5 != undefined){
 		document.getElementById("Q5").value = localStorage.Q5;
 	}
+
 	switch(localStorage.Q1){
 		case "o1":
 		document.getElementById("o1").checked = true;
@@ -175,7 +217,8 @@
 		var regForm = document.getElementById("regForm")
 		regForm.onsubmit = validate;
 		prefill_form();
-		
+		displayDetails();
+		calcScore();
 	}
 
 	window.onload = init;
