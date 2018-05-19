ddf5fc0ba0c5adaaf123a48c0c872d42d9a7eda7
HARDIK CHUGH <31733278+hardy1334@users.noreply.github.com>
2018-05-19 09:07:25 +0530

    Update result.js

--- result.js
+++ result.js
@@ -9,28 +9,16 @@
 "use strict";
 
 function validate(){
-	
 	var errMsg = "";								
 	var result = true;						
-
-
 	return result;    
 }
 
 //This should be really be calculated securely on the server! 
-function calcScore(Q1){
-	var score = 0;
-	if (score("o1") != -1) score =+ 0;
-	if (score("o2")!= -1) score += 2;
-	if (score("o3")!= -1) score += 0;
-	return score;
-	
-}
+
 
 function getScore(){
-	//Var score = 0;
-	
-	//   (localStorage.firstname != undefined){    
+	   
 		document.getElementById("confirm_name").textContent = localStorage.firstname + " " + localStorage.lastname;
 		document.getElementById("confirm_Student_ID").textContent =localStorage.Student_ID;
 		document.getElementById("confirm_Q1").textContent =localStorage.Q1;
@@ -39,15 +27,21 @@ function getScore(){
 		document.getElementById("confirm_Q4").textContent =localStorage.Q4;
 		document.getElementById("confirm_Q5").textContent =localStorage.Q5;
 		
-		score = calcScore(localStorage.Q1);
-		document.getElementById("confirm_score").textContent = score;
-	
-		document.getElementById("firstname").value = localStorage.firstname;
-			
-		document.getElementById("score").value = score;
+				document.getElementById("firstname").value = localStorage.firstname;
+
+
 	}
+function scr(){
+	var getmrk=localStorage.getItem("getmarks");
+	var strmarks=document.getElementById("confirm_score");
+	strmarks.innerHTML=getmrk;
+	var name=document.getElementById("confirm_Q5").value=localStorage.Q5;
+	//var name=localStorage.Student_ID;
+	console.log(localStorage.Q5);
+}
+function getq1(){
 
-
+}
 
 function cancelBooking(){
 	window.location = "quiz.html";
@@ -57,8 +51,8 @@ function init () {
 	//var regForm = document.getElementById("regForm");// link the variable to the HTML element
 	//regForm.onsubmit = validate;          /* assigns functions to corresponding events */
 	getScore();
-	document.getElementById("cancelButton").onclick = cancelBooking;
-	
+	document.getElementById("cancelButton").onClick = cancelBooking;
+scr();
  }
 
 window.onload = init;
